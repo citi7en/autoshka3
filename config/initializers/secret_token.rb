810f52a29ca5acdc171b380dff7ab9b4bad933a6
citi7en <ryakhov.ivan@gmail.com>
2011-07-10 20:09:30 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autoshka3::Application.config.secret_token = '5c2fa43f2393ce3e8c641bc65c21e63dfbe2a17a68a7a0479a68b7ad493335d46b06e0cceef20b525edc13fcff928259378674e184f4085cb19b33a6c57454d0'
